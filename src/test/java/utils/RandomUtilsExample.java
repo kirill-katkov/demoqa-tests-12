58f22eb0c556d3d587cd63b96e80edb7abb18196
Kirill Katkov <k.katkov@dodopizza.com>
2022-04-11 09:25:12 +0300

    update .gitignore

--- src/test/java/utils/RandomUtilsExample.java
+++ src/test/java/utils/RandomUtilsExample.java
@@ -5,7 +5,7 @@ import static utils.RandomUtils.*;
 public class RandomUtilsExample {
     public static void main(String[] args) {
         System.out.println("Random string of 10 letters: "+ getRandomString(10));
-        System.out.println("Random int: "+ getRandomInt(1234,999999));
+        System.out.println("Random int: "+ getRandomInt(1234,999999));//значение
         System.out.println("Random email "+ getRandomEmail());
     }
 }
