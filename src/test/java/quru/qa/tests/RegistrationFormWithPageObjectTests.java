30a8142b06d64cc949861eada1922c62e7ad51de
Kirill Katkov <k.katkov@dodopizza.com>
2022-04-11 09:29:47 +0300

    Update v2

--- src/test/java/quru/qa/tests/RegistrationFormWithPageObjectTests.java
+++ src/test/java/quru/qa/tests/RegistrationFormWithPageObjectTests.java
@@ -56,6 +56,7 @@ public class RegistrationFormWithPageObjectTests {
 
     @Test
     void fillFormTest() {
+
         registrationFormPage.openPage().
                 setFirstName(firstName)
                 .setLastName(lastName)
